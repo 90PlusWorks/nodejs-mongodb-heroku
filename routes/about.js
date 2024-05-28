d9182103999bc7f93dc0f384426a0008948e155d
90PlusWorks <102657193+90PlusWorks@users.noreply.github.com>
2024-05-28 10:48:37 +0200

    Update about.js

--- routes/about.js
+++ routes/about.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('about', {
-        body: 'The webpage is so awfull because I dont like css'
+        body: '90PlusWorks /br --It works--'
     });
 });
 
